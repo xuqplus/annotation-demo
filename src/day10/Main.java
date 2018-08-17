8d557cfc66efb42d310573c5f78010d4041d35a3
xuqplus <445172495@qq.com>
2018-08-17 14:40:50 +0800

    checkAccessible

--- src/day10/Main.java
+++ src/day10/Main.java
@@ -27,5 +27,41 @@ public class Main {
         Method[] declaredMethods = User.class.getDeclaredMethods(); // 所有内部定义的
         Annotation[] annotations = User.class.getAnnotations();
         Annotation[] declaredAnnotations = User.class.getDeclaredAnnotations();
+
+        deal(user, fields, declaredFields);
+        deal(user, methods, declaredMethods);
+    }
+
+    private static void deal(User user, Field[] fields, Field[] declaredFields) {
+        /**
+         * 每个二进制位所表示的修饰符
+         * 查看 java.lang.reflect.Modifier
+         */
+        /**
+         * field.setAccessible(true);
+         * 查了下, 不是改modifiers的, 而是禁用"安全性检查"的, 禁用了在执行反射时可以省很多时间
+         * 至于到底标查了什么没细看, |  --> java.lang.reflect.AccessibleObject
+         */
+        for (Field field : fields) {
+            try {
+                System.out.println(String.format("declaredFields, name=%s, modifiers=%s, value=%s", field.getName(), field.getModifiers(), field.get(user)));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        for (Field field : declaredFields) {
+            try {
+                System.out.println(String.format("declaredFields, name=%s, modifiers=%s, value=%s", field.getName(), field.getModifiers(), field.get(user)));
+            } catch (IllegalAccessException e) {
+                System.err.println(String.format("declaredFields, name=%s, modifiers=%s", field.getName(), field.getModifiers()));
+                // e.printStackTrace();
+            }
+        }
+    }
+
+    private static void deal(User user, Method[] methods, Method[] declaredMethods) {
+        for (Method method : methods) {
+            System.err.println(method);
+        }
     }
 }
